f39bca8f6fa17af0494dfde65be065e8b7e3efe8
landonrobinson <toonboy1>
2014-02-18 17:15:05 -0500

    Baby's first push.

---
+++ ExplicitIntentExample/src/com/example/explicitintentexample/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.explicitintentexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+		
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.second, menu);
+		return true;
+	}
+
+}
